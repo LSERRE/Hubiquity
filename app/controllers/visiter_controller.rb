69947cf07f1226a21377dec397dcb314dbb70a96
Louis Serre <louis.serre@gmail.com>
2015-06-16 17:40:04 +0200

    Accept or Refuse offers

--- app/controllers/visiter_controller.rb
+++ app/controllers/visiter_controller.rb
@@ -1,9 +1,24 @@
 class VisiterController < ApplicationController
- def add
-    user_id = current_user.id
+	def add
+		user_id = current_user.id
 
-    @relation = Visiter.create(user_id: user_id, visit_id: params[:visit_id], visitDate: params[:visitDate], visitTime: params[:visitTime], confirmed: 0)
-    @relation.save
-    redirect_to visit_index_path
-  end
+		@relation = Visiter.create(user_id: user_id, visit_id: params[:visit_id], visitDate: params[:visitDate], visitTime: params[:visitTime], confirmed: "pending")
+		@relation.save
+		redirect_to visit_index_path
+	end
+
+	def accept
+		visiter = Visiter.find(params[:visiter_id])
+		visiter.confirmed = 'accepted'
+		visiter.save
+		redirect_to dashboard_path
+	end
+
+	def refuse
+		visiter = Visiter.find(params[:visiter_id])
+		visiter.confirmed = 'refused'
+		visiter.save
+		redirect_to dashboard_path
+	end
+
 end
--- app/models/visiter.rb
+++ app/models/visiter.rb
@@ -1,6 +1,6 @@
 class Visiter < ActiveRecord::Base
-	belongs_to :visit, :class_name => :Visit,:foreign_key => "visit_id"
-    belongs_to :user, :class_name => :User,:foreign_key => "user_id"
+	belongs_to :visit, :class_name => :Visit, :foreign_key => "visit_id"
+    belongs_to :visiter, :class_name => :User, :foreign_key => "user_id"
 
     validates :visit_id, presence: true
     validates :user_id, presence: true
