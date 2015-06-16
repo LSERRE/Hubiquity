b52e6e064d3196bca848b44975a5cd45160c2d63
Louis Serre <louis.serre@gmail.com>
2015-06-17 01:58:17 +0200

    Format Date + Some fixes

--- app/helpers/visit_helper.rb
+++ app/helpers/visit_helper.rb
@@ -1,2 +1,5 @@
 module VisitHelper
+	def parseDatetime(date, time)
+    	dateTime = DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec).to_formatted_s(:short)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
 	     :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "default.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	has_many :visit
 	has_many :missions, :foreign_key => 'visit_id', :class_name => "User", through: :visiter
