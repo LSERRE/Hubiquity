5e29fde0b4b173bbb587cb7daa2d211e887b2eba
Louis Serre <louis.serre@gmail.com>
2015-06-17 16:34:18 +0200

    Visit reports

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,13 @@ class DashboardController < ApplicationController
 	def index
 		@visits = Visit.where(user_id: current_user.id)
 		@potential_visiters = []
+		@confirmed_visits = []
 		@visits.each do |visit|
 			visiter = Visiter.where( visit_id: visit.id, confirmed: "pending" )
 			@potential_visiters.push( visiter )
+
+			confirmed = Visiter.where( visit_id: visit.id, confirmed: "confirmed" )
+			@confirmed_visits.push( confirmed )
 		end
 	end
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,14 @@
 class ReportsController < ApplicationController
 	def index
 		@visits = Visit.where(user_id: current_user.id)
-		@potential_visiters = []
-		@visits.each do |visit|
-			visiter = Visiter.where( visit_id: visit.id, confirmed: "pending" )
-			@potential_visiters.push( visiter )
+		@pending_visits = []
+		@visits.each_with_index do |visit, index|
+			@pending_visits[index] = []
+			@pending_visits[index][0] = visit
+			visiters = Visiter.where( visit_id: visit.id, confirmed: "pending" )
+			@pending_visits[index][1] = visiters
 		end
+
+		@confirmed_visiters = Visiter.where( confirmed: "accepted" )
 	end
 end
