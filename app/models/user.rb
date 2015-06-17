18e0b8fd092f050f7e14acb831e16d6c13d36c5a
martindebray <martin.debray@gmail.com>
2015-06-17 14:42:44 +0200

    louis il veut merger

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
 	     :recoverable, :rememberable, :trackable, :validatable
 
-  	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/default.png"
+  	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	has_many :visit
