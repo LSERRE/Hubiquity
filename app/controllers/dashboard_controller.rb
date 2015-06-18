fcb0facf07c2c5d1f37e5a8107d91ecb5078e2d9
Louis Serre <louis.serre@gmail.com>
2015-06-18 15:57:25 +0200

    Dashobard account

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
 	def index
 		@visits = Visit.where(user_id: current_user.id)
+
 		@visiters = []
 		@confirmed_visiters = Visiter.where( requester_id: current_user.id, confirmed: "accepted" )
 		@visits.each_with_index do |visit, index|
@@ -9,5 +10,6 @@ class DashboardController < ApplicationController
 			pending_visiters = Visiter.where( visit_id: visit.id, confirmed: "pending" )
 			@visiters[index][1] = pending_visiters
 		end
+
 	end
 end
